ac29512012e15e3fe1bf6c8e4ffd6edd419e3130
Jerson <jer.armijo@alumnos.duoc.cl>
2020-12-08 13:33:20 -0300

    Refactorizado

--- therexmecanic/Azure/ClienteAzure.cs
+++ therexmecanic/Azure/ClienteAzure.cs
@@ -17,34 +17,53 @@ namespace therexmecanic.Azure
         //obtener todos los clientes
         public static List<Cliente> ObtenerClientes()
         {
-            var dataTable = new DataTable();
+            
             using (SqlConnection sqlConnection = new SqlConnection(connectionString))
             {
-                SqlCommand sqlCommand = new SqlCommand(null, sqlConnection);
-                sqlCommand.CommandText = "select * from Cliente";
+
+                var comando = AbrirConexionSqlClientes(sqlConnection);
+                var dataTable = LlenadoTabla(comando);
+                return Listarclientes(dataTable);
+
+            }
+            
+        }
+
+        public static SqlCommand AbrirConexionSqlClientes(SqlConnection sqlConnection)
+        {
+            SqlCommand sqlCommand = new SqlCommand(null, sqlConnection);
+            sqlCommand.CommandText = "select * from Cliente";
+            sqlConnection.Open();
+            return sqlCommand;
+        }
+
+        public static DataTable LlenadoTabla(SqlCommand comando)
+        {
+            var dataTable = new DataTable();
+            var dataAdapter = new SqlDataAdapter(comando);
+            dataAdapter.Fill(dataTable);
+            return dataTable;
+        }
+
+        public static List<Cliente> Listarclientes(DataTable dataTable)
+        {
+            clientes = new List<Cliente>();
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                Cliente cliente = new Cliente();
+                cliente.CodCliente = int.Parse(dataTable.Rows[i]["codCliente"].ToString());
+                cliente.Nombre = dataTable.Rows[i]["Nombre"].ToString();
+                cliente.Apellido = dataTable.Rows[i]["Apellido"].ToString();
+                cliente.Edad = int.Parse(dataTable.Rows[i]["Edad"].ToString());
+                cliente.Email = dataTable.Rows[i]["Email"].ToString();
+                cliente.Domicilio = dataTable.Rows[i]["Domicilio"].ToString();
+                cliente.Telefono = int.Parse(dataTable.Rows[i]["Telefono"].ToString());
+                clientes.Add(cliente);
                 
-                sqlConnection.Open();
-
-                var dataAdapter = new SqlDataAdapter(sqlCommand);
-                dataAdapter.Fill(dataTable);
-
-                clientes = new List<Cliente>();
-
-                for (int i = 0; i < dataTable.Rows.Count; i++)
-                {
-                    Cliente cliente = new Cliente();
-                    cliente.CodCliente = int.Parse(dataTable.Rows[i]["codCliente"].ToString());
-                    cliente.Nombre = dataTable.Rows[i]["Nombre"].ToString();
-                    cliente.Apellido = dataTable.Rows[i]["Apellido"].ToString();
-                    cliente.Edad = int.Parse(dataTable.Rows[i]["Edad"].ToString());
-                    cliente.Email = dataTable.Rows[i]["Email"].ToString();
-                    cliente.Domicilio = dataTable.Rows[i]["Domicilio"].ToString();
-                    cliente.Telefono = int.Parse(dataTable.Rows[i]["Telefono"].ToString());
-                    clientes.Add(cliente);
-                }
             }
             return clientes;
         }
+    
     }
 
 }
