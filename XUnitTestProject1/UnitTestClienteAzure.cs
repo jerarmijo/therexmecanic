522d1c94334444719b402f210e714650d6cacfc1
Jerson <jer.armijo@alumnos.duoc.cl>
2020-12-08 12:27:19 -0300

    creada conexion a bdd primer metodo cliente y prueba unitarias

---
+++ XUnitTestProject1/UnitTestClienteAzure.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using System.Linq;
+
+
+namespace XUnitTestProject1
+{
+    public class UnitTestClienteAzure
+    {
+        [Fact]
+        public void TestObtenerClientes()
+        {
+            //arrange
+            bool vieneConDatos = false;
+            //act
+            ClienteAzure.
+            //assert
+
+        }
+
+    }
+}
---
+++ therexmecanic/Azure/ClienteAzure.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using therexmecanic.Models;
+
+namespace therexmecanic.Azure
+{
+    public class ClienteAzure
+    {
+        //ruta
+        static string connectionString = @"Server=DESKTOP-A88JHQV\MSSQLSERVER1;Database=DBtherexmecanic;Trusted_Connection=True;";
+        private static List<Cliente> clientes;
+        
+        //obtener todos los clientes
+        public static List<Cliente> ObtenerClientes()
+        {
+            var dataTable = new DataTable();
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(null, sqlConnection);
+                sqlCommand.CommandText = "select * from Cliente";
+                
+                sqlConnection.Open();
+
+                var dataAdapter = new SqlDataAdapter(sqlCommand);
+                dataAdapter.Fill(dataTable);
+
+                clientes = new List<Cliente>();
+
+                for (int i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    Cliente cliente = new Cliente();
+                    cliente.CodRegistro = int.Parse(dataTable.Rows[i]["codRegistro"].ToString());
+                    cliente.Nombre = dataTable.Rows[i]["Nombre"].ToString();
+                    cliente.Apellido = dataTable.Rows[i]["Apellido"].ToString();
+                    cliente.Edad = int.Parse(dataTable.Rows[i]["Edad"].ToString());
+                    cliente.Email = dataTable.Rows[i]["Email"].ToString();
+                    cliente.Domicilio = dataTable.Rows[i]["Domicilio"].ToString();
+                    cliente.Telefono = int.Parse(dataTable.Rows[i]["Telefono"].ToString());
+                    clientes.Add(cliente);
+                }
+            }
+            return clientes;
+        }
+    }
+
+}
