b3eac5c025e4fcfc6d38ff371a08720dedae6853
Jerson <jer.armijo@alumnos.duoc.cl>
2020-12-09 14:59:30 -0300

    agregado metodos de ehiculo y controlador

---
+++ therexmecanic/Controllers/VehiculoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using therexmecanic.Azure;
+using therexmecanic.Models;
+
+namespace therexmecanic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiculoController : ControllerBase
+    {
+
+        [HttpGet("Listado")]
+        public JsonResult obtenerVehiculos()
+        {
+            var vehiculosRetornados = VehiculoAzure.ObtenerVehiculos();
+            return new JsonResult(vehiculosRetornados);
+        }
+
+
+
+    }
+}
