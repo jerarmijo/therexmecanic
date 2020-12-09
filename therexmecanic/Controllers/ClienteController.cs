c184fdf12766bcf97bc29c1ea43fdc1cc2d90c27
Jerson <jer.armijo@alumnos.duoc.cl>
2020-12-09 00:31:03 -0300

    se agregan metodos  de busqueda por id y nombre en el controlador

--- therexmecanic/Controllers/ClienteController.cs
+++ therexmecanic/Controllers/ClienteController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using therexmecanic.Azure;
+using therexmecanic.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,5 +20,34 @@ namespace therexmecanic.Controllers
             var clientesRetornados = ClienteAzure.ObtenerClientes();
             return new JsonResult(clientesRetornados);
         }
+
+        [HttpGet("{Cliente}")]
+        public JsonResult ObtenerCliente(string Cliente)
+        {
+            var conersionExitosa = int.TryParse(Cliente, out int codigoConvertido);
+            Cliente clienteRetornado;
+
+            if (conersionExitosa)
+            {
+                clienteRetornado = ClienteAzure.ObtenerClienteID(codigoConvertido);
+            }
+            else
+            {
+                clienteRetornado = ClienteAzure.ObtenerCliente(Cliente);
+            }
+            if (clienteRetornado is null)
+            {
+                return new JsonResult($"No se encontraron registros para : {Cliente}. Intente nuevamente");
+            }
+            else
+            {
+                return new JsonResult(clienteRetornado);
+            }
+
+            
+            
+        }
     }
+
+
 }
